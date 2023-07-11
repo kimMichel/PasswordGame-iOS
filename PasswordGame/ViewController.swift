6448e28faa946b60a60935743ccb433280db0072
Michel Kim <kim98.michel@gmail.com>
2023-07-11 14:41:56 -0300

    Initial commit

---
+++ PasswordGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PasswordGame
+//
+//  Created by Michel Jaejin Kim on 11/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# PasswordGame
+  iOS Project, where the game "PasswordGame" is a game to create your password according to the validations required by the game in each level.
