77a533de048b047c33d8cacd9378f17299f39052
Michel Kim <kim98.michel@gmail.com>
2023-07-25 16:30:49 -0300

    Delete ValidationManager

--- PasswordGame/src/Controller/ViewController.swift
+++ PasswordGame/src/Controller/ViewController.swift
@@ -7,29 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController, ValidationManagerDelegate {
-
-    @IBOutlet var passwordLabel: UITextField!
-    @IBOutlet var table: UITableView!
-    @IBOutlet var validationLabel: UILabel!
-    
-    var validationManager = ValidationManager()
-    
-    var level = 1 {
-        didSet {
-            title = "Level: \(level)"
-        }
-    }
+class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        validationManager.delegate = self
-        
-        validationManager.startGame()
-    }
-
-    func didUpdateValidationLabel(validation: String) {
-        validationLabel.text = validation
     }
 
 }
